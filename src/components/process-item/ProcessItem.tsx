78f37640540250c866bc740171a5c20b3addd198
Ilia Lesik <ilialesik@gmail.com>
2019-07-16 23:30:08 +0300

    Add some styles

--- src/components/process-item/ProcessItem.tsx
+++ src/components/process-item/ProcessItem.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import {ProcessDescriptor} from "ps-list";
+import "./processItem.css"
+
+const toFixed2 = (num) => Math.round(num * 100) / 100;
 
 export const ProcessItem = (props: ProcessDescriptor & {key: number}) => {
     const {name, cpu, memory} = props;
-    return <div>
-        {name} {cpu && cpu * 100} {memory && memory * 100}
+    return <div className="processItem">
+        <div className="processItem__column processItem__name">
+            <span className="processItem__text">
+                {name}
+            </span>
+        </div>
+        <div className="processItem__column">
+            <span className="processItem__text">
+                {toFixed2(cpu && cpu * 100)}%
+            </span>
+        </div>
+        <div className="processItem__column">
+            <span className="processItem__text">
+                {toFixed2(memory && memory * 100)}%
+            </span>
+        </div>
     </div>
 };
--- src/components/process-list/ProcessList.tsx
+++ src/components/process-list/ProcessList.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import {useStore} from "effector-react";
 import {processListStore} from "../../processListStore";
 import {ProcessItem} from "../process-item/ProcessItem";
+import "./processList.css"
 
 export const ProcessList = () => {
     const items = useStore(processListStore);
 
-    return <div>
+    return <div class="processList">
         {items.map((item) => <ProcessItem {...item} key={item.pid}  />)}
     </div>
 };
