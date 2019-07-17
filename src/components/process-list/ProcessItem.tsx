1b0dd5acb98ca0698a31a29911de65fafbe6d6cb
Ilia Lesik <ilialesik@gmail.com>
2019-07-17 17:30:35 +0300

    Apply new styles to process list

--- src/components/process-list/ProcessItem.tsx
+++ src/components/process-list/ProcessItem.tsx
@@ -5,32 +5,28 @@ import "./processItem.css"
 const toFixed2 = (num) => Math.round(num * 100) / 100;
 
 export const ProcessItem = (props: ProcessDescriptor & {key: number}) => {
-    const {name, cpu, memory, pid, ppid} = props;
+    const {name, cpu, memory} = props;
     return <div className="processItem">
         <div className="processItem__column processItem__name">
-            <span className="processItem__text">
+            <span className="processItem__text processItem__text--blue">
                 {name}
             </span>
         </div>
-        <div className="processItem__column">
-            <span className="processItem__text">
-                {pid}
-            </span>
-        </div>
-        <div className="processItem__column">
-            <span className="processItem__text">
-                {ppid}
-            </span>
-        </div>
-        <div className="processItem__column">
-            <span className="processItem__text">
-                {toFixed2(cpu && cpu * 100)}%
-            </span>
-        </div>
-        <div className="processItem__column">
-            <span className="processItem__text">
-                {toFixed2(memory && memory * 100)}%
-            </span>
-        </div>
+        {typeof cpu === "number" && <div className="processItem__column">
+            <p class="processItem__label">
+                cpu
+            </p>
+            <p className={`processItem__text ${(cpu * 100)< 50 ? 'processItem__text--green' : 'processItem__text--red'}`}>
+                {toFixed2(cpu * 100)}%
+            </p>
+        </div>}
+        {typeof memory === "number" && <div className="processItem__column">
+            <p class="processItem__label">
+                memory
+            </p>
+            <p className={`processItem__text ${(memory * 100) < 50 ? 'processItem__text--green' : 'processItem__text--red'}`}>
+                {toFixed2(memory * 100)}%
+            </p>
+        </div>}
     </div>
 };
