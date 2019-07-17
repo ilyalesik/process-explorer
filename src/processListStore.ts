362d35c6b6c73d13143b8aea0730fd739cdfd344
Ilia Lesik <ilialesik@gmail.com>
2019-07-17 10:54:40 +0300

    Refactor process list store

--- src/processListStore.ts
+++ src/processListStore.ts
@@ -1,10 +1,6 @@
-import {createEvent, createStore} from "effector";
+import {createEvent, restore} from "effector";
 import {ProcessDescriptor} from "ps-list";
-
-export const $processList = createStore<Array<ProcessDescriptor>>([]);
 
 export const updateListEvent = createEvent<Array<ProcessDescriptor>>("updateListEvent");
 
-$processList.on(updateListEvent, (_, newValue) => {
-    return newValue;
-});
+export const $processList = restore(updateListEvent, []);
