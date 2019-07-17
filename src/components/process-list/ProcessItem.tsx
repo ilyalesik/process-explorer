78e25674f6cfd825bcf97aeafa8d194bd68ba39e
Ilia Lesik <ilialesik@gmail.com>
2019-07-17 11:02:10 +0300

    Implement showing pid, ppid at ProcessItem

--- src/components/process-list/ProcessItem.tsx
+++ src/components/process-list/ProcessItem.tsx
@@ -5,11 +5,21 @@ import "./processItem.css"
 const toFixed2 = (num) => Math.round(num * 100) / 100;
 
 export const ProcessItem = (props: ProcessDescriptor & {key: number}) => {
-    const {name, cpu, memory} = props;
+    const {name, cpu, memory, pid, ppid} = props;
     return <div className="processItem">
         <div className="processItem__column processItem__name">
             <span className="processItem__text">
                 {name}
+            </span>
+        </div>
+        <div className="processItem__column">
+            <span className="processItem__text">
+                {pid}
+            </span>
+        </div>
+        <div className="processItem__column">
+            <span className="processItem__text">
+                {ppid}
             </span>
         </div>
         <div className="processItem__column">
