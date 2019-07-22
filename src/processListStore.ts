41b9a165f7fbab7fdc3bff942e1a9f35d0e09a6a
Ilia Lesik <ilialesik@gmail.com>
2019-07-23 00:03:24 +0300

    Fix TS errors at processListStore

--- src/processListStore.ts
+++ src/processListStore.ts
@@ -4,14 +4,14 @@ import * as R from "ramda"
 
 export const updateListEvent = createEvent<Array<ProcessDescriptor>>("updateListEvent");
 
-export const updateListEventMap = updateListEvent.map(R.indexBy(R.prop('pid')));
+export const updateListEventMap = updateListEvent.map(R.indexBy(R.compose(R.toString, R.prop('pid'))));
 export const $processListMap = restore(updateListEventMap, {});
 
 
 const updateListParentMap = updateListEvent.map(
     R.compose(
         R.mapObjIndexed(R.map(R.prop('pid'))),
-        R.groupBy(R.prop('ppid'))
+        R.groupBy(R.compose(R.toString, R.prop('ppid')))
     )
 );
 export const $parentMap = restore(updateListParentMap, {});
